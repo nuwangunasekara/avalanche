4d008187b24d65065f34fe22776f9f717c8b349f
Nuwan Gunasekara <ng198002@gmail.com>
2021-09-20 21:17:10 +1200

    get results from multiple runs

--- exp_scripts/get_results_from_csv.py
+++ exp_scripts/get_results_from_csv.py
@@ -1,21 +1,26 @@
 import pandas as pd
+import numpy as np
 import re
 import subprocess
 import argparse
 
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+# pd.set_option('display.max_colwidth', -1)
+
 parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--resultsDir", type=str, help="Results directory", default='/Users/ng98/Desktop/avalanche_test/exp/logs')
+parser.add_argument("-r", "--resultsDir", type=str, help="Results directory", default='/Users/ng98/Desktop/avalanche_test/results/')
 args = parser.parse_args()
 
 csv_files = []
 if len(csv_files) == 0:
-    command = subprocess.Popen("find " + args.resultsDir + "/csv_data/ -iname 'eval_results.csv'",
+    command = subprocess.Popen("find " + args.resultsDir + " -iname '*eval_results.csv'",
                                shell=True, stdout=subprocess.PIPE)
     for line in command.stdout.readlines():
         csv_files.append(line.decode("utf-8").replace('\n', ''))
 print(csv_files)
 
-print('dataset,strategy,sub_strategy,eval_accuracy,forgetting')
 df_final_results = pd.DataFrame(columns=['dataset', 'strategy', 'sub_strategy', 'eval_accuracy', 'forgetting'])
 
 for csv_file in csv_files:
@@ -29,18 +34,32 @@ for csv_file in csv_files:
         sub_strategy = match.group(0)
     else:
         sub_strategy = 'NA'
-    eval_after_training_last = df.query("eval_exp == 0 & training_exp == " + str(df['training_exp'].max()))
-    eval_accuracy = eval_after_training_last.iloc[0, eval_after_training_last.columns.get_loc('eval_accuracy')]
-    forgetting = eval_after_training_last.iloc[0, eval_after_training_last.columns.get_loc('forgetting')]
 
-    new_row = {'dataset': dataset, 'strategy': strategy, 'sub_strategy': sub_strategy, 'eval_accuracy': sub_strategy, 'forgetting': forgetting}
+    last_exp_id = str(df['training_exp'].max())
+    tmp_pd = df.query("eval_exp == 0 and training_exp == " + last_exp_id).copy(deep=True)
+    tmp_pd.loc[:, 'dataset'] = dataset
+    tmp_pd.loc[:, 'strategy'] = strategy
+    tmp_pd.loc[:, 'sub_strategy'] = sub_strategy
 
-    df_final_results = df_final_results.append(new_row, ignore_index=True)
-    print('{},{},{},{},{}'.format(
-        dataset,
-        strategy,
-        sub_strategy,
-        eval_accuracy,
-        forgetting))
+    df_final_results = df_final_results.append(tmp_pd, ignore_index=True)
+
+print(df_final_results)
 
 df_final_results.to_csv(args.resultsDir+'/Final_results.csv', header=True, index=False)
+
+pd_forgetting = pd.pivot_table(
+    df_final_results, values='forgetting', index=['dataset'], columns=['strategy', 'sub_strategy'], aggfunc=np.mean, fill_value=0)
+pd_eval_accuracy = pd.pivot_table(
+    df_final_results, values='eval_accuracy', index=['dataset'], columns=['strategy', 'sub_strategy'], aggfunc=np.mean, fill_value=0)
+
+pd_forgetting_std = pd.pivot_table(
+    df_final_results, values='forgetting', index=['dataset'], columns=['strategy', 'sub_strategy'], aggfunc=np.std, fill_value=0)
+pd_eval_accuracy_std = pd.pivot_table(
+    df_final_results, values='eval_accuracy', index=['dataset'], columns=['strategy', 'sub_strategy'], aggfunc=np.std, fill_value=0)
+
+with pd.ExcelWriter(args.resultsDir+'/Final_results.xlsx') as writer:
+    pd_eval_accuracy.to_excel(writer, sheet_name='AccAfterLast', index=True)
+    pd_eval_accuracy_std.to_excel(writer, sheet_name='AccAfterLastS', index=True)
+    pd_forgetting.to_excel(writer, sheet_name='Forgetting', index=True)
+    pd_forgetting_std.to_excel(writer, sheet_name='ForgettingS', index=True)
+
