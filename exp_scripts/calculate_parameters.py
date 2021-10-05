6e24cbc05364f5f50c57914c7391f428c797d0e5
Nuwan Gunasekara <ng198002@gmail.com>
2021-10-05 16:35:43 +1300

    Update calculate_parameters.py

--- exp_scripts/calculate_parameters.py
+++ exp_scripts/calculate_parameters.py
@@ -1,3 +1,20 @@
+from prettytable import PrettyTable
+
+
+def count_parameters(model):
+    table = PrettyTable(["Modules", "Parameters"])
+    total_params = 0
+    for name, parameter in model.named_parameters():
+        if not parameter.requires_grad:
+            continue
+        param = parameter.numel()
+        table.add_row([name, param])
+        total_params+=param
+    print(table)
+    print(f"Total Trainable Params: {total_params}")
+    return total_params
+
+
 def new_dem(i, p, f, s):
     return abs(((i + (2 * p) - f) // s) + 1)
 
@@ -68,4 +85,5 @@ CNN = [
 print_info(SimpleCNN)
 print_info(CNN)
 
+# count_parameters(model)
 
