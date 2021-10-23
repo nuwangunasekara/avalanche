92ad8bc4ae167d4b191d6f091548ebec6755be8e
Nuwan Gunasekara <ng198002@gmail.com>
2021-10-24 00:24:22 +1300

    Support for auto_detect_tasks experiments

--- exp_scripts/run_exp.sh
+++ exp_scripts/run_exp.sh
@@ -19,6 +19,7 @@ mini_batch_size='16'
 tp_pool_type='6CNN'
 tp_number_of_nns_to_train='6'
 tp_predict_methods_array=('ONE_CLASS' 'ONE_CLASS_end' 'MAJORITY_VOTE' 'RANDOM' 'NAIVE_BAYES' 'NAIVE_BAYES_end' 'TASK_ID_KNOWN')
+tp_predict_methods_array=('ONE_CLASS')
 #tp_reset_tp='reset'
 tp_reset_tp='no_reset'
 #tp_use_one_class_probas='no_use_probas'
