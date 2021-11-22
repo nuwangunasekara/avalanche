517dde463196ffb491234565a5bea7fd4d6fcbd5
Nuwan Gunasekara <ng198002@gmail.com>
2021-11-23 08:13:48 +1300

    Add task id detection at the end plot

--- exp_scripts/network_info.py
+++ exp_scripts/network_info.py
@@ -174,19 +174,75 @@ def plot_fozen_nw_stats(csv_file, eval_csv_file, excel_writer, d, ax):
     ax.annotate('avg acc after last ' + str( avg_acc_after_last) + '%, avg forgetting after last ' + str( avg_forgetting_after_last) + ', frozen nets at the end=' + str(frozen_nw_count_at_end), (0, y_max))
 
 
+def plot_fozen_nw_stats_at_the_end(csv_file, excel_writer, d, ax):
+    df = pd.read_csv(csv_file)
+    df_frozen = df.query('dumped_at.str.contains("after_eval") and list_type.str.contains("frozen_net")',
+                         engine='python')
+    last_t_id = df['training_exp'].max()
+    df_frozen_nw_at_end = df.query(
+        'dumped_at.str.contains("after_eval") and list_type.str.contains("frozen_net") and training_exp==' + str(
+            last_t_id),
+        engine='python')
+
+    df_frozen_nw_at_end = df_frozen_nw_at_end[[
+        'training_exp', 'dumped_at', 'detected_task_id', 'list_type', 'this_frozen_id',
+        'samples_per_each_task_at_train', 'this_seen_task_ids_train', 'this_correctly_predicted_task_ids_test_at_last', 'this_correctly_predicted_task_ids_probas_test_at_last',
+        'correct_network_selected_count_at_last', 'instances_per_task_at_last']]
+
+    df_empty = pd.DataFrame({'this_frozen_id': [], 'type': []})
+    task_ids = []
+    for t in df['training_exp'].unique():
+        task_ids.append(t)
+
+    frozen_idx = {}
+    f_idx = 0
+    for f in df_frozen_nw_at_end['this_frozen_id'].unique():
+        frozen_idx[f] = str(f_idx)
+        f_idx += 1
+
+    group_cols = {
+        # 'samples_per_each_task_at_train': 'at_tr',
+        'this_seen_task_ids_train': '_t',
+        'this_correctly_predicted_task_ids_test_at_last': 'y^',
+        'this_correctly_predicted_task_ids_probas_test_at_last': 'p'}
+
+    for i in df_frozen_nw_at_end.index.values:
+        for k, c in group_cols.items():
+            t_ids = ast.literal_eval(df_frozen.at[i, k])
+            t_ids['type'] = c
+            t_ids['this_frozen_id'] = frozen_idx[df_frozen.at[i, 'this_frozen_id']]
+            tmp_df = pd.DataFrame.from_records([t_ids])
+            df_empty = df_empty.append(tmp_df, ignore_index=True)
+
+    df_empty.to_excel(excel_writer, sheet_name=d+'_frozen_last', index=False)
+    df_empty = df_empty.groupby(['this_frozen_id', 'type'])[task_ids].max()
+    ax.set_ylabel(d)
+    df_empty.plot(kind='bar', stacked=False, ax=ax)
+    for label in ax.get_xticklabels():
+        label.set_rotation(45)
+        label.set_ha('right')
+    ax.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0)
+
 # Start of the main
 def main():
     exp = get_experiment_dir().split('_')[-1].split('/')[0]
     fig_1 = plt.figure(constrained_layout=False, figsize=(18, 10))
     fig_1.suptitle('task detection (at train) experiment# ' + exp)
-    fig_2 = plt.figure(constrained_layout=False, figsize=(18, 10))
-    fig_2.suptitle('correct nw selected Vs. correct class detected (at eval) experiment# ' + exp)
-    fig_3 = plt.figure(constrained_layout=False, figsize=(18, 10))
-    fig_3.suptitle('correct task_id predicted % by each frozen nw, after training on each task (at eval) experiment# '
-                   + exp + '\n (trained task, frozen at task id_ detected id_nw id)')
+    # fig_2 = plt.figure(constrained_layout=False, figsize=(18, 10))
+    # fig_2.suptitle('correct nw selected Vs. correct class detected (at eval) experiment# ' + exp)
+    # fig_3 = plt.figure(constrained_layout=False, figsize=(18, 10))
+    # fig_3.suptitle('correct task_id predicted % by each frozen nw, after training on each task (at eval) experiment# '
+    #                + exp + '\n (trained task, frozen at task id_ detected id_nw id)')
+    fig_4 = plt.figure(constrained_layout=False, figsize=(18, 10))
+    fig_4.suptitle('For each task, accumulated in-class (1s) predictions (y^) and probabilities (p) by each frozen network(at test),'
+                   '\nalso the number of instances seen by each frozen network during training.'
+                   # '\nexperiment# ' + exp
+                   )
+
     gs_1 = fig_1.add_gridspec(len(datasets), 1)
-    gs_2 = fig_2.add_gridspec(len(datasets), 1)
-    gs_3 = fig_2.add_gridspec(len(datasets), 1)
+    # gs_2 = fig_2.add_gridspec(len(datasets), 1)
+    # gs_3 = fig_3.add_gridspec(len(datasets), 1)
+    gs_4 = fig_4.add_gridspec(len(datasets), 1)
     rows = 0
     cols = 0
     with pd.ExcelWriter(args.resultsDir + '/NW_used_for_prediction_'+exp+'.xlsx') as excel_writer:
@@ -198,17 +254,24 @@ def main():
             eval_csv_file = get_eval_csv_file(d_name)
 
             ax_1 = fig_1.add_subplot(gs_1[rows, cols], label=d_name)
-            ax_2 = fig_2.add_subplot(gs_2[rows, cols], label=d_name)
-            ax_3 = fig_3.add_subplot(gs_3[rows, cols], label=d_name)
+            # ax_2 = fig_2.add_subplot(gs_2[rows, cols], label=d_name)
+            # ax_3 = fig_3.add_subplot(gs_3[rows, cols], label=d_name)
+            ax_4 = fig_4.add_subplot(gs_4[rows, cols], label=d_name)
             plot_task_detection(net_csv_file, d_name, ax_1)
-            plot_network_selection(net_csv_file, excel_writer, d_name, ax_2)
-            plot_fozen_nw_stats(net_csv_file, eval_csv_file, excel_writer, d_name, ax_3)
+            # plot_network_selection(net_csv_file, excel_writer, d_name, ax_2)
+            # plot_fozen_nw_stats(net_csv_file, eval_csv_file, excel_writer, d_name, ax_3)
+            plot_fozen_nw_stats_at_the_end(net_csv_file, excel_writer, d_name, ax_4)
             rows += 1
 
+    ax_4.set_xlabel('(frozen_nn_id, type)' +
+                    '\nFor given task, types: y^ = Accumulated in-class prediction(1)s at test,'
+                    ' p = Accumulated in-class probabilities at test,'
+                    ' _t = number of instances seen at train')
     mplcursors.cursor(hover=True)
     fig_1.savefig(args.resultsDir+'/TaskDetection_' + exp + '.png')
-    fig_2.savefig(args.resultsDir+'/CorrectNWSelected_' + exp + '.png')
-    fig_3.savefig(args.resultsDir+'/CorrectTaskIDPredicted_' + exp + '.png')
+    # fig_2.savefig(args.resultsDir+'/CorrectNWSelected_' + exp + '.png')
+    # fig_3.savefig(args.resultsDir+'/CorrectTaskIDPredicted_' + exp + '.png')
+    fig_4.savefig(args.resultsDir + '/CorrectTaskIDPredicted_at_the_end_' + exp + '.png')
     plt.show()
 
 
