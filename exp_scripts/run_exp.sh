24ae2bd05fd5d09020b4939079d9b67532513848
Nuwan Gunasekara <ng198002@gmail.com>
2022-02-13 11:46:25 +1300

    Use GDumb instead of ER

--- exp_scripts/run_exp.sh
+++ exp_scripts/run_exp.sh
@@ -12,7 +12,7 @@ dataset=(LED_a RotatedMNIST RotatedCIFAR10 CORe50 CLStream51)
 dataset=(CORe50 RotatedMNIST RotatedCIFAR10 )
 #dataset=(CORe50)
 strategy=(LwF EWC GDumb ER TrainPool)
-strategy=(TrainPool ER)
+strategy=(TrainPool GDumb)
 
 mini_batch_size='16'
 
