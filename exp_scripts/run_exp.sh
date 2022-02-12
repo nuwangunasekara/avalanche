244477c9884903ce4fde651980dae11e9a4d9b40
Nuwan Gunasekara <ng198002@gmail.com>
2022-02-13 11:47:45 +1300

    set dataset to CLStream51

--- exp_scripts/run_exp.sh
+++ exp_scripts/run_exp.sh
@@ -10,7 +10,7 @@ clean_dir="TRUE"
 
 dataset=(LED_a RotatedMNIST RotatedCIFAR10 CORe50 CLStream51)
 dataset=(CORe50 RotatedMNIST RotatedCIFAR10 )
-#dataset=(CORe50)
+dataset=(CLStream51)
 strategy=(LwF EWC GDumb ER TrainPool)
 strategy=(TrainPool GDumb)
 
