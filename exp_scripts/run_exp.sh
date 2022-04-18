28c89b01c8dac792a3422a152ec87601414b43e9
Nuwan Gunasekara <ng198002@gmail.com>
2022-04-19 11:54:16 +1200

    Simple CNN experiments

--- exp_scripts/run_exp.sh
+++ exp_scripts/run_exp.sh
@@ -56,7 +56,7 @@ tp_skip_back_prop_threshold='0.0'
 #tp_skip_back_prop_threshold='0.3'
 
 tp_adwin_delta_in_log10='-3.0'
-tp_max_frozen_pool_size='3'
+tp_max_frozen_pool_size='-1'
 per_task_mem_buff_size='300'
 
 model='SimpleCNN'
